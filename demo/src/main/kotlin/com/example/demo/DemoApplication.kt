1222d159c32532ee1eb48d1a25c176834ffbaa3e
thanhlt-1007 <le.tan.thanh@sun-asterisk.com>
2023-08-14 18:03:18 +0700

    Implement MessageController

--- demo/src/main/kotlin/com/example/demo/DemoApplication.kt
+++ demo/src/main/kotlin/com/example/demo/DemoApplication.kt
@@ -3,9 +3,19 @@ package com.example.demo
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
 
+import org.springframework.web.bind.annotation.RestController
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.RequestParam
+
 @SpringBootApplication
 class DemoApplication
 
 fun main(args: Array<String>) {
 	runApplication<DemoApplication>(*args)
+}
+
+@RestController
+class MessageController {
+	@GetMapping("/")
+	fun index(@RequestParam("name") name: String) = "Hello, $name!"
 }
